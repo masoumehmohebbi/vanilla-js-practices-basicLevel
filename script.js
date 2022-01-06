fd8f56e7db97756a1a0eb66c3e816b468e20da88
MasoumeMohebbi <88876100+MasoumeMohebbi@users.noreply.github.com>
2022-01-06 14:00:49 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let getUserNumber
+let myArray = []
+let firstNumber 
+let SecondNumber 
+let sumDigit = 0 
+let sumAllNumbers =0
+
+for(let i = 0 ; i < 3 ; i++){
+
+   getUserNumber = Number(prompt('please Insert Your ' + (i + 1) + ' Number'))
+
+   myArray.push(getUserNumber)
+   firstNumber = myArray[0]
+   SecondNumber = myArray[1]
+
+   sumAllNumbers+= getUserNumber
+  
+   }
+
+   for (let j = 0 ; (firstNumber / 10) != 0 ; j++){
+        
+        sumDigit += (firstNumber % 10)
+        firstNumber = Math.floor(firstNumber / 10)
+    }
+    
+alert('Sum Of FirstNumber Digis is : ' + sumDigit)           // Sum digits Of First Number
+
+alert('The Muximum Number Is : ' + Math.max(getUserNumber))  // Muximum Numbers
+
+alert ('Sum Of all three Numbers Is : ' + sumAllNumbers)     // Sum of All Numbers
+
+alert ('Square Of Second Number Is : ' + Math.sqrt(SecondNumber)) //Square Of Second Number
+     
+
+
+
+
+
+
+
+
+ 
+
