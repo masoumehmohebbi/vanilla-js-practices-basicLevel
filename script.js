664424ab38317e7bc58b261dd261e1d2760ec827
MasoumeMohebbi <88876100+MasoumeMohebbi@users.noreply.github.com>
2022-01-11 11:29:24 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿let MynumbersArray = [
+  {id:1, number:2},
+  {id:2, number:13},
+  {id:3, number:84},
+  {id:4, number:39},
+]
+
+MynumbersArray.forEach(num =>{
+  if (num.number % 2 == 0) {
+console.log('Even Numbers In MyNumbersArray => ', num.number)
+    
+  }
+} )
