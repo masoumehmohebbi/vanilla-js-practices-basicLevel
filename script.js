03f76458372f038dfc22156ea805c69a452107da
MasoumeMohebbi <88876100+MasoumeMohebbi@users.noreply.github.com>
2022-01-25 13:14:31 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-﻿var carStore = [
-	{id: 1, name: 'Bugatti', price: 5000000, color:'black'},
-	{id: 2, name: 'Pagani HUAYRA BC', price: 3000000, color:'white'},
-	{id: 3, name: 'Rolls Royse', price: 85000, color:'green'},
-	{id: 4, name: 'Mercedes-Maybachive', price: 94000, color:'black'},
-	{id: 5, name: 'Koenigsegg', price: 84000, color:'black'},
-]
+﻿let userArray = prompt('Enter Your String : ')      // "Masoume" |string
+userArray= (userArray.split(''))                  //["M", "a", "s", "u","m","e"]  |convrt to Array
+let ArrayLowerCase
 
-var filteredCarsColor = carStore.filter(function (car) {  //Finding The Items with black color
-	return car.color === 'black'
-})
-// ////////
-let blackCarsName = filteredCarsColor.map(car => {   	
-return (car.name)
-});
-console.log('The Cars Name With Black Colour >>> ', blackCarsName)
-// ////////////
-let carsPrice=[]
-let carsName=[]
-filteredCarsColor.forEach(car => {
-	carsPrice.push(car.price) 
-	carsName.push(car.name)
-});
+let isArray = (Array.isArray(userArray))       //true   
+if (isArray) {	   					    	  //check for being an array
+	
+	console.log(('Your Array >>> ', userArray))
 
-const SortedLengthOfCarName = carsName.sort((a,b) => a.length - b.length);
+	
+	ArrayLowerCase = userArray.join('').toLowerCase().split('') //convert To LowerCase
+	
+	if ((ArrayLowerCase.indexOf("m"))=== -1) {
+		console.log('There Isn\'t the characters Of M in Your String')
+	}else{
+		console.log(ArrayLowerCase.indexOf('m'))
+	}
 
-console.log('Sorted Black Car Name with Length >>> ',SortedLengthOfCarName)
-console.log('Sort with smallest first digit Of Black-Car Prices >>> ',carsPrice.sort())  
-console.log('The Price Of Expensive Black-Car >>> ', Math.max(...carsPrice))    
+	
+(userArray.length > 2) ? console.table("The Last Two characters >>>", userArray.slice(-2).join('')): null
+	
+}
+
 
