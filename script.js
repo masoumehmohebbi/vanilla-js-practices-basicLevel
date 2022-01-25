9304321ef6a07530f245338b7c924764d12cd196
MasoumeMohebbi <88876100+MasoumeMohebbi@users.noreply.github.com>
2022-01-25 13:12:52 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿var carStore = [
+	{id: 1, name: 'Bugatti', price: 5000000, color:'black'},
+	{id: 2, name: 'Pagani HUAYRA BC', price: 3000000, color:'white'},
+	{id: 3, name: 'Rolls Royse', price: 85000, color:'green'},
+	{id: 4, name: 'Mercedes-Maybachive', price: 94000, color:'black'},
+	{id: 5, name: 'Koenigsegg', price: 84000, color:'black'},
+]
+
+var filteredCarsColor = carStore.filter(function (car) {  //Finding The Items with black color
+	return car.color === 'black'
+})
+// ////////
+let blackCarsName = filteredCarsColor.map(car => {   	
+return (car.name)
+});
+console.log('The Cars Name With Black Colour >>> ', blackCarsName)
+// ////////////
+let carsPrice=[]
+let carsName=[]
+filteredCarsColor.forEach(car => {
+	carsPrice.push(car.price) 
+	carsName.push(car.name)
+});
+
+const SortedLengthOfCarName = carsName.sort((a,b) => a.length - b.length);
+
+console.log('Sorted Black Car Name with Length >>> ',SortedLengthOfCarName)
+console.log('Sort with smallest first digit Of Black-Car Prices >>> ',carsPrice.sort())  
+console.log('The Price Of Expensive Black-Car >>> ', Math.max(...carsPrice))    
+
