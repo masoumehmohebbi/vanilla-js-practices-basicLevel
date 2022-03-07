cbb5e18ab1276f468042530378d27fbe985b605e
masoume-mohebbi <masoumeh.mohebbi@gmail.com>
2022-03-07 18:52:30 +0330

    added files of practice-19

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿
+let timerText = document.querySelector('p')
+let i=0
+
+
+
+let timer = setInterval(() => {
+
+	i++
+	timerText.innerHTML = i;
+
+	if (timerText.innerHTML >= 4) {
+		clearInterval(timer)
+		timerText.innerHTML =" "
+	}
+
+
+}, 1000);
+
+setTimeout(() => {
+	timerText.innerHTML = "GO!"
+}, 5000);
