8ada5dd6a72efada205cea944067289ac96d9d01
masoume-mohebbi <masoumeh.mohebbi@gmail.com>
2022-03-07 18:12:27 +0330

    added project-18

---
+++ js/script.js
@@ -0,0 +1,12 @@
+﻿let stringArray=["Hello","This", "Is", "Your", "Website"],
+    textBox = document.querySelector('p'),
+    i =0
+
+let timer = setInterval(() => {
+	textBox.append(stringArray[i] + " ")
+	i++
+	if (i > stringArray.length){
+		i = 0
+		textBox.innerHTML=""
+	}
+}, 1000);
